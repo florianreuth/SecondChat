d14ab79ba2981c79b1cdbd8d0302406e7212af9c
Florian Reuth <git@florianreuth.de>
2026-03-28 11:12:23 +0100

    Initial steps towards Gradle version catalogs
    Signed-off-by: Florian Reuth <git@florianreuth.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,12 +8,18 @@ plugins {
 setupProject()
 setupFabric()
 setupPublishing()
-includeFabricApiModules("fabric-api-base", "fabric-resource-loader-v0", "fabric-screen-api-v1", "fabric-lifecycle-events-v1")
 
 repositories {
     maven("https://maven.terraformersmc.com/releases")
 }
 
+val jij = configureJij()
+
 dependencies {
+    jij(fabricApi.module("fabric-api-base", fabricApiVersion))
+    jij(fabricApi.module("fabric-resource-loader-v0", fabricApiVersion))
+    jij(fabricApi.module("fabric-screen-api-v1", fabricApiVersion))
+    jij(fabricApi.module("fabric-lifecycle-events-v1", fabricApiVersion))
+
     compileOnly("com.terraformersmc:modmenu:18.0.0-alpha.8")
 }
