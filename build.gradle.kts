9ae3e980fb846f32f38e0e842b9cf7c97be75182
Florian Reuth <git@florianreuth.de>
2026-08-03 16:28:56 +0200

    Update BaseProject
    Signed-off-by: Florian Reuth <git@florianreuth.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
-import de.florianreuth.baseproject.*
+import de.florianreuth.baseproject.integration.configureJarInJar
+import de.florianreuth.baseproject.integration.fabricApiVersion
+import de.florianreuth.baseproject.integration.setupFabric
+import de.florianreuth.baseproject.setupProject
+import de.florianreuth.baseproject.setupPublishing
 
 plugins {
     id("net.fabricmc.fabric-loom")
@@ -13,13 +17,13 @@ repositories {
     maven("https://maven.terraformersmc.com/releases")
 }
 
-val jij = configureJij()
+val shade = configureJarInJar()
 
 dependencies {
-    jij(fabricApi.module("fabric-api-base", fabricApiVersion))
-    jij(fabricApi.module("fabric-resource-loader-v0", fabricApiVersion))
-    jij(fabricApi.module("fabric-screen-api-v1", fabricApiVersion))
-    jij(fabricApi.module("fabric-lifecycle-events-v1", fabricApiVersion))
+    shade(fabricApi.module("fabric-api-base", fabricApiVersion))
+    shade(fabricApi.module("fabric-resource-loader-v0", fabricApiVersion))
+    shade(fabricApi.module("fabric-screen-api-v1", fabricApiVersion))
+    shade(fabricApi.module("fabric-lifecycle-events-v1", fabricApiVersion))
 
     compileOnly("com.terraformersmc:modmenu:20.0.0-beta.2")
 }
