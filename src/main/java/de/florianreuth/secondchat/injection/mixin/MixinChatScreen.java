15eaf21b20ad837fdfb79eebb6f24f5603a36adf
Florian Reuth <git@florianreuth.de>
2026-08-03 16:29:53 +0200

    Update copyright header
    Signed-off-by: Florian Reuth <git@florianreuth.de>

--- src/main/java/de/florianreuth/secondchat/injection/mixin/MixinChatScreen.java
+++ src/main/java/de/florianreuth/secondchat/injection/mixin/MixinChatScreen.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of SecondChat - https://github.com/florianreuth/SecondChat
- * Copyright (C) 2025-2026 Florian Reuth <git@florianreuth.de> and contributors
+ * Copyright (C) 2025-2026 Florian Reuth <git@florianreuth.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
